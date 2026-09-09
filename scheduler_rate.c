488ab399acb5b24fb4fad4923ab9f2b43d32150b
Vinicius Cezar <vcrc@cesar.school>
2026-09-08 22:11:56 -0300

    feat: implementa laço principal de simulação instante a instante

--- scheduler_rate.c
+++ scheduler_rate.c
@@ -24,3 +24,52 @@ int escolher_proxima_rate(Task tarefas[], int n, int instante_atual) {
 
     return escolhida;
 }
+
+int simular_rate(Task tarefas[], int n, int tempo_total, Evento eventos[]) {
+    int num_eventos = 0;
+    int executando_atual = -1;
+    int duracao_atual = 0;
+
+    for (int t = 0; t < tempo_total; t++) {
+        for (int i = 0; i < n; i++) {
+            if (tarefas[i].proxima_chegada == t) {
+                tarefas[i].rajada_restante = tarefas[i].burst;
+                tarefas[i].deadline_absoluto = t + tarefas[i].deadline;
+            }
+        }
+
+        int escolhida = escolher_proxima_rate(tarefas, n, t);
+
+        if (escolhida == executando_atual) {
+            duracao_atual++;
+        } else {
+            if (duracao_atual > 0) {
+                eventos[num_eventos].eh_ociosidade = (executando_atual == -1);
+                eventos[num_eventos].indice_tarefa = executando_atual;
+                eventos[num_eventos].duracao = duracao_atual;
+                num_eventos++;
+            }
+            executando_atual = escolhida;
+            duracao_atual = 1;
+        }
+
+        if (escolhida != -1) {
+            tarefas[escolhida].rajada_restante--;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (tarefas[i].proxima_chegada == t) {
+                tarefas[i].proxima_chegada = t + tarefas[i].period;
+            }
+        }
+    }
+
+    if (duracao_atual > 0) {
+        eventos[num_eventos].eh_ociosidade = (executando_atual == -1);
+        eventos[num_eventos].indice_tarefa = executando_atual;
+        eventos[num_eventos].duracao = duracao_atual;
+        num_eventos++;
+    }
+
+    return num_eventos;
+}
--- scheduler_rate.h
+++ scheduler_rate.h
@@ -3,6 +3,14 @@
 
 #include "task.h"
 
+typedef struct {
+    int eh_ociosidade;
+    int indice_tarefa;
+    int duracao;
+} Evento;
+
 int escolher_proxima_rate(Task tarefas[], int n, int instante_atual);
+
+int simular_rate(Task tarefas[], int n, int tempo_total, Evento eventos[]);
 
 #endif
