56b4856d0958c93e09b0c53ef4fcf3f65a6077ce
Vinicius Cezar <vcrc@cesar.school>
2026-09-08 21:45:32 -0300

    feat: implementa leitura das tarefas do arquivo de entrada

--- io.c
+++ io.c
@@ -17,3 +17,35 @@ int ler_tempo_total(const char *caminho_arquivo) {
 
     return tempo_total;
 }
+
+int ler_tarefas(const char *caminho_arquivo, Task tarefas[]) {
+    FILE *f = fopen(caminho_arquivo, "r");
+    if (f == NULL) {
+        return -1;
+    }
+
+    int tempo_total;
+    if (fscanf(f, "%d", &tempo_total) != 1) {
+        fclose(f);
+        return -1;
+    }
+
+    int quantidade = 0;
+    while (quantidade < MAX_TAREFAS &&
+           fscanf(f, "%31s %d %d %d",
+                  tarefas[quantidade].nome,
+                  &tarefas[quantidade].period,
+                  &tarefas[quantidade].deadline,
+                  &tarefas[quantidade].burst) == 4) {
+
+        tarefas[quantidade].ordem_arquivo = quantidade;
+        tarefas[quantidade].proxima_chegada = 0;
+        tarefas[quantidade].deadline_absoluto = tarefas[quantidade].deadline;
+        tarefas[quantidade].rajada_restante = tarefas[quantidade].burst;
+
+        quantidade++;
+    }
+
+    fclose(f);
+    return quantidade;
+}
--- io.h
+++ io.h
@@ -3,6 +3,10 @@
 
 #include "task.h"
 
+#define MAX_TAREFAS 32
+
 int ler_tempo_total(const char *caminho_arquivo);
+
+int ler_tarefas(const char *caminho_arquivo, Task tarefas[]);
 
 #endif
